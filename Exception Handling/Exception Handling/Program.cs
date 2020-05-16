578a03568919e2d0422b7d86474990022e4d88aa
BayoOlawumi <olawumiebayo@gmail.com>
2020-05-16 15:22:01 +0100

    Commits after class

--- Exception Handling/Exception Handling/Program.cs
+++ Exception Handling/Exception Handling/Program.cs
@@ -9,51 +9,74 @@ namespace Exception_Handling
         static void Main(string[] args)
         {
 
-        //    string decision = String.Empty;
-        //    do
+            //*******************************Example 1*****************************//
+            double val = 0;
+            try
+            {
+                int age = int.Parse(Console.ReadLine());
+                val = 12 / age;
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You entered zero");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("There is a problem which am not sure of, contact admin \n " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(val.ToString());
+                Console.ReadKey();
+            }
+
+
+
+            //try
+            //{
+            //    Console.WriteLine("Enter your age");
+            //    int val = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(val * 0.8);
+
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("You have a wrong value");
+            //    throw;
+            //}
+            //Console.ReadKey();
+        }
+        //string decision = String.Empty;
+        //do
+        //{
+        //    Console.WriteLine("Please Enter a binary value");
+        //    string num = Console.ReadLine();
+        //    int length = num.Length;
+        //    double Answer = 0;
+
+        //    foreach (var number in num)
         //    {
-        //        Console.WriteLine("Please Enter a binary value");
-        //        string num = Console.ReadLine();
-        //        int length = num.Length;
-        //        double Answer = 0;
-        //        Console.WriteLine("This is " + num);
-
-        //        foreach (var number in num)
+        //        int power = length - 1;
+        //        if (number != '1' && number != '0')
         //        {
-        //            int power = length - 1;
-        //            if (number != '1' && number != '0')
-        //            {
-        //                Console.WriteLine("This number is not a binary");
-        //                break;
-        //            }
-        //            else
-        //            {
-        //                Answer += Math.Pow(2, power) * double.Parse(number.ToString());
-        //                length--;
-        //            }
+        //            Console.WriteLine("This number is not a binary");
+        //            break;
         //        }
-        //        Console.WriteLine("The equaivalent value of {0} is {1}", num, Answer);
-        //        Console.WriteLine("Do you want to continue conversion, write yes or no");
-        //        decision = Console.ReadLine();
-        //    } while (decision.ToLower() == "yes");
+        //        else
+        //        {
+
+        //            Answer += Math.Pow(2, power) * double.Parse(number.ToString());
+        //            length--;
+        //        }
+        //    }
+        //    Console.WriteLine("The equaivalent value of {0} is {1}", num, Answer);
+        //    Console.WriteLine("Do you want to continue with conversion, write yes or no");
+        //    decision = Console.ReadLine();
+        // } while (decision.ToLower() == "yes");
         //}
 
-        //*******************************Example 1*****************************//
-        //double val = 0;
-        //try
-        //{
-        //    int age = int.Parse(Console.ReadLine());
-        //    val = 12 / age;
 
-        //}
-        //catch(DivideByZeroException e)
-        //{
-        //    Console.WriteLine("You entered zero");
-        //}
-        //finally{
-        //    Console.WriteLine(val.ToString());
-        //    Console.ReadKey();
-        //}
 
         //int a = int.Parse(Console.ReadLine());
         //if(a<5)
